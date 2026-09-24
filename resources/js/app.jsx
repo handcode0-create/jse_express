2b55ba8516618f34f6fe83c920113b210328751f
handcode0-create <handcode0@gmail.com>
2026-09-24 17:20:12 +0000

    feat: afficher un état de chargement global Inertia

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,10 @@
 import "../css/app.css";
 
+import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ChargementPage } from "./Composants/Interface/EtatsChargement";
 
 createInertiaApp({
     title: (title) => `${title} - JSE Express`,
@@ -14,7 +16,7 @@ createInertiaApp({
         ),
 
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />);
+        createRoot(el).render(<Suspense fallback={<ChargementPage />}><App {...props} /></Suspense>);
     },
 
     progress: {
