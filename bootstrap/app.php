c2f4eb4ea17429f88ee0b75d8cebba6cea000c8b
handcode0-create <handcode0@gmail.com>
2026-09-24 18:41:56 +0000

    fix: utiliser la redirection des invités compatible Laravel 13

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
-        $middleware->redirectGuestsTo('/authentification');
+        $middleware->redirectGuestsTo(fn () => '/authentification');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
