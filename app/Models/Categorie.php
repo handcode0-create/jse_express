4b632fde7e8f91a1bd62e7d9f3d7accda04572a8
handCode <handcode0@gmail.com>
2026-09-25 12:14:50 +0000

    fix: activer HasFactory sur les modèles JSE

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -10,7 +10,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie extends Model
 {
 
-    public $timestamps = false;
+    
+    use HasFactory;
+public $timestamps = false;
     protected $fillable = [
         'restaurant_id',
         'nom',
