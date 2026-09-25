3f1c927a2982fd5a0177f53ab0cb0a6785b0b439
handCode <handcode0@gmail.com>
2026-09-25 12:15:01 +0000

    fix: activer HasFactory sur les modèles JSE

--- app/Models/LigneCommande.php
+++ app/Models/LigneCommande.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class LigneCommande extends Model
 {
-    public $timestamps = false;
+    
+    use HasFactory;
+public $timestamps = false;
     protected $fillable = [
         'commande_id',
         'produit_id',
